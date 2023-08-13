5f83bf73c937a9872d432a2664f83d776e033d0c
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-13 17:07:23 +0330

    Initial Commit

---
+++ CoffeeList/CoffeeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeListApp.swift
+//  CoffeeList
+//
+//  Created by mohsen mokhtari on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
